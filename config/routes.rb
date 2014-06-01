f48911234c86cc12e04d972c340becb82ab24b02
dineshkarthik <dinesh.kartoon@gmail.com>
2014-06-01 03:00:18 -0700

    Comments Included

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	root to: 'articles#index'
-	resources :articles
+	resources :articles do
+		resources :comments
+	end
 	resources :authors
 	resources :author_sessions, only: [ :new, :create, :destroy ]
 	get 'login'  => 'author_sessions#new'
